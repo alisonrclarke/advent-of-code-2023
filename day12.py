d82fde93a6195a63cd2764434d949fb69f5096e5
Alison Clarke <a.clarke@wellcome.org>
2023-12-16 07:42:32 +0000

    Some black formatting

--- day12.py
+++ day12.py
@@ -6,42 +6,44 @@ import utils
 
 test_mode = len(sys.argv) > 1
 if test_mode:
-    suffix = sys.argv[2] if len(sys.argv) > 2 else ''
-    input_file = f'day12_test_input{suffix}.txt'
+    suffix = sys.argv[2] if len(sys.argv) > 2 else ""
+    input_file = f"day12_test_input{suffix}.txt"
 else:
-    input_file = f'day12_input.txt'
+    input_file = f"day12_input.txt"
 
 data = utils.input_as_lines(input_file)
 
+
 def matches_sizes(s, group_sizes):
-    p = r'^\.*'
-    for (i, g) in enumerate(group_sizes):
-        p += rf'#{{{g}}}'
+    p = r"^\.*"
+    for i, g in enumerate(group_sizes):
+        p += rf"#{{{g}}}"
         if i == len(group_sizes) - 1:
-            p += r'\.*$'
+            p += r"\.*$"
         else:
-            p += r'\.+'
+            p += r"\.+"
 
     # if re.match(p, s):
     # print(s, group_sizes, p, re.match(p, s))
 
     return re.match(p, s)
 
+
 @functools.cache
 def permutations(n):
     if n == 1:
-        return ['#', '.']
+        return ["#", "."]
     else:
         perms = []
         for p in permutations(n - 1):
-            perms.extend([['#', *p], ['.', *p]])
+            perms.extend([["#", *p], [".", *p]])
         return perms
 
 
 @functools.cache
 def find_permutations(s, group_sizes):
     chars = list(s)
-    query_indexes = [i for i, c in enumerate(chars) if c == '?']
+    query_indexes = [i for i, c in enumerate(chars) if c == "?"]
     perms = permutations(len(query_indexes))
     total = 0
     checked = []
@@ -49,19 +51,20 @@ def find_permutations(s, group_sizes):
         char_list = chars.copy()
         for i, idx in enumerate(query_indexes):
             char_list[idx] = p[i]
-        s2 = ''.join(char_list)
-        if s2 not in checked and matches_sizes(''.join(char_list), group_sizes):
+        s2 = "".join(char_list)
+        if s2 not in checked and matches_sizes("".join(char_list), group_sizes):
             total += 1
             checked.append(s)
 
     return total
+
 
 @functools.cache
 def find_permutations2(s, group_sizes):
     # Massive thanks to u/StaticMoose for the tutorial on this!
     # My original method (above) was far too slow.
     if len(group_sizes) == 0:
-        if '#' in s:
+        if "#" in s:
             # No groups left
             return 0
         else:
@@ -69,10 +72,10 @@ def find_permutations2(s, group_sizes):
     elif len(s) == 0:
         return 0
 
-    if s[0] == '#':
+    if s[0] == "#":
         # Need to match first group size chars as #s
         # If string is too short or there are any dots in there, there's no match
-        if '.' in s[:group_sizes[0]] or len(s) < group_sizes[0]:
+        if "." in s[: group_sizes[0]] or len(s) < group_sizes[0]:
             return 0
         elif len(s) == group_sizes[0]:
             if len(group_sizes) == 1:
@@ -81,29 +84,31 @@ def find_permutations2(s, group_sizes):
                 return 0
         else:
             if s[group_sizes[0]] in "?.":
-                return find_permutations2(s[group_sizes[0]+1:], tuple(group_sizes[1:]))
+                return find_permutations2(
+                    s[group_sizes[0] + 1 :], tuple(group_sizes[1:])
+                )
             else:
                 return 0
-    elif s[0] == '.':
+    elif s[0] == ".":
         return find_permutations2(s[1:], group_sizes)
     else:
-        hash_perms = find_permutations2('#' + s[1:], group_sizes)
-        dot_perms = find_permutations2('.' + s[1:], group_sizes)
+        hash_perms = find_permutations2("#" + s[1:], group_sizes)
+        dot_perms = find_permutations2("." + s[1:], group_sizes)
         return hash_perms + dot_perms
 
 
 p1_total = 0
 for line in data:
-    s, cond = line.split(' ')
-    group_sizes = tuple([int(c) for c in cond.split(',')])
+    s, cond = line.split(" ")
+    group_sizes = tuple([int(c) for c in cond.split(",")])
     p1_total += find_permutations2(s, group_sizes)
 
 print(f"Part 1: {p1_total}")
 
 p2_total = 0
 for i, line in enumerate(data):
-    s, cond = line.split(' ')
-    group_sizes = tuple([int(c) for c in cond.split(',')])
-    p2_total += find_permutations2('?'.join([s]*5), group_sizes*5)
+    s, cond = line.split(" ")
+    group_sizes = tuple([int(c) for c in cond.split(",")])
+    p2_total += find_permutations2("?".join([s] * 5), group_sizes * 5)
 
 print(f"Part 2: {p2_total}")
--- day14.py
+++ day14.py
@@ -5,37 +5,40 @@ import utils
 
 test_mode = len(sys.argv) > 1
 if test_mode:
-    suffix = sys.argv[2] if len(sys.argv) > 2 else ''
-    input_file = f'day14_test_input{suffix}.txt'
+    suffix = sys.argv[2] if len(sys.argv) > 2 else ""
+    input_file = f"day14_test_input{suffix}.txt"
 else:
-    input_file = f'day14_input.txt'
+    input_file = f"day14_input.txt"
 
 data = utils.input_as_lines(input_file)
+
 
 @functools.cache
 def move_to_start(s: str, reverse_=False):
     if reverse_:
         l = list(s)
         l.reverse()
-        s = ''.join(l)
+        s = "".join(l)
 
-    splits = s.split('#')
+    splits = s.split("#")
     new_splits = []
     for s2 in splits:
-        n = len([c for c in s2 if c == 'O'])
-        new_splits.append('O'*n + '.'*(len(s2)-n))
+        n = len([c for c in s2 if c == "O"])
+        new_splits.append("O" * n + "." * (len(s2) - n))
 
-    new_s = '#'.join(new_splits)
+    new_s = "#".join(new_splits)
     if reverse_:
         l = list(new_s)
         l.reverse()
-        return ''.join(l)
+        return "".join(l)
     else:
         return new_s
+
+
 def get_score(s):
     total = 0
     for i, c in enumerate(s):
-        if c == 'O':
+        if c == "O":
             # print(f"rock in row {i}, score {len(s) - i}")
             total += len(s) - i
     return total
@@ -85,7 +88,7 @@ for n in range(1000000000):
     # Back to cols for start
     cols = transpose(rows)
 
-    cycle_string = ''.join(cols)
+    cycle_string = "".join(cols)
     if cycle_string in cycles:
         # We have a loop! Can work out final state from here
         cycle_index = cycles.index(cycle_string)
@@ -93,7 +96,10 @@ for n in range(1000000000):
         remaining_cycles = 1000000000 - n
         cycles_to_add = remaining_cycles % cycle_length
         last_state = cycles[cycles_to_add - cycle_length - 1]
-        final_cols = [last_state[i:i+len(cols[0])] for i in range(0, len(last_state), len(cols[0]))]
+        final_cols = [
+            last_state[i : i + len(cols[0])]
+            for i in range(0, len(last_state), len(cols[0]))
+        ]
         break
 
     cycles.append(cycle_string)
